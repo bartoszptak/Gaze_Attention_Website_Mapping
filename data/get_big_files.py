f3ffc626e4fefe6b4de62f1b1ee5e0626c239b95
Bartosz Ptak <bartptak@gmail.com>
2019-07-09 11:30:00 +0200

    Add zip files with model and dataset

---
+++ data/get_big_files.py
@@ -0,0 +1,60 @@
+import requests
+import zipfile
+import os
+
+
+def download_file_from_google_drive(id, destination):
+    def get_confirm_token(response):
+        for key, value in response.cookies.items():
+            if key.startswith('download_warning'):
+                return value
+
+        return None
+
+    def save_response_content(response, destination):
+        CHUNK_SIZE = 32768
+
+        with open(destination, "wb") as f:
+            for chunk in response.iter_content(CHUNK_SIZE):
+                if chunk:  # filter out keep-alive new chunks
+                    f.write(chunk)
+
+    print('GET: {}'.format(destination))
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params={'id': id}, stream=True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = {'id': id, 'confirm': token}
+        response = session.get(URL, params=params, stream=True)
+
+    save_response_content(response, destination)
+
+def download_unzip_and_remove(google_code, name):
+    download_file_from_google_drive(
+        google_code, name)
+
+    with zipfile.ZipFile(name, 'r') as zip_ref:
+        zip_ref.extractall('.')
+
+    os.remove(name)
+
+
+if __name__ == "__main__":
+    import sys
+
+    # face predictor
+    download_file_from_google_drive(
+        '1TXJn_tAKkgmg9aMAVUrY8E2A9xUpoxLl', 'shape_predictor_68_face_landmarks.dat')
+
+    # tensorflow model
+    download_unzip_and_remove('1wPEBjl6NjpQOhh-J2ZoR3XxxUPb7do4B', 'model.zip')
+
+    # datasets
+    download_unzip_and_remove('1AQ-ToGm4-PG2HlEdnvVEzX73sf-XOBL5', 'dataset.zip')
+
+    # train logs
+    download_unzip_and_remove('1DyHAYc3qOjl4odaeI9YgW82PhTV5ZVHE', 'train.zip')
